fc7e76171c28d236b3dcef5be73e1831a7fac6ef
Benyeemin <xverb01@gjk.cz>
2021-04-06 17:45:33 +0200

    Add a separate function for creating SQL commands

--- method1_SQL.py
+++ method1_SQL.py
@@ -4,6 +4,10 @@ from pathlib import Path
 from psycopg2.extras import DictCursor
 from nltk.corpus import wordnet
 
+def build_search_sql(text, column):
+    cases = [f"case when {column} like '%{word}%' then 1 else 0 end" for word in text]
+    return ' + '.join(cases)
+
 def search(text):
     for word in text:
         word = ''.join([i if ord(i) < 128 else '_' for i in word])
@@ -15,15 +19,8 @@ def search(text):
 
     server_info = json.loads(open(str(server_info_path)).readline())
 
-    command = "select key, "
-    first = True
-    for word in text:
-        if first:
-            command = command + "case when name like '%" + word + "%' then 1 else 0 end"
-            first = False
-        else:
-            command = command + " + case when name like '%" + word + "%' then 1 else 0 end"
-    command = command + ' as words from authors order by words desc limit 1;'
+    command = build_search_sql(text, 'name')
+    command = "select key, " + command + ' as words from authors order by words desc limit 1;'
     try:
         conn = psycopg2.connect(host=server_info["host"], user=server_info["user"], password=server_info["password"],
                                 database=server_info["database"], sslmode='require', cursor_factory=DictCursor)
@@ -40,10 +37,8 @@ def search(text):
         if not wordnet.synsets(word):
             text.remove(word)
 
-    command = 'select title, main_author, '
-    for word in text:
-        command = command + "case when title like '%" + word + "%' then 1 else 0 end + "
-    command = command + "case when main_author='" + author_key + "' then " + str(max(
+    command = build_search_sql(text, 'title')
+    command = 'select title, main_author, ' + command + " + case when main_author='" + author_key + "' then " + str(max(
         len(text) // 3, 1)) + ' else 0 end as words from works order by words desc limit 1;'
     with closing(conn.cursor()) as cursor:
         cursor.execute(command)
